9e8ade54612e0ad9860f45965fb9582970a19b66
mogobams <m.mogoba.mm@gmail.com>
2020-02-20 15:47:42 +0200

    adding .csproj, .cs and .sln files to the repo

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,20 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        static void Prefix(string input)
+        {
+            int length = input.Length;
+            int words = input.Split(' ').Length;
+
+            Console.WriteLine(length + "," + words + ":" + input);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "what  ... did you say?";
+            Prefix(name);
         }
     }
 }
