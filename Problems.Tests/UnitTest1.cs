89c6146ec6c3c029400584994bdd5451e4251357
mogobams <m.mogoba.mm@gmail.com>
2020-02-24 22:43:00 +0200

    single test method added

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -1,8 +1,11 @@
 using NUnit.Framework;
+using System;
+using System.Collections;
 using Problems;
 
 namespace Problems.Tests
 {
+    [TestFixture]
     public class Tests
     {
         [SetUp]
@@ -10,28 +13,54 @@ namespace Problems.Tests
         {
         }
 
-        [Test]
-        public void Test1()
+        [TestCaseSource(typeof(MyTestClass), "TestCases")]
+        public string Prefix(string input)
         {
-            Program p = new Program();
-            string output = p.Prefix("hello");
-            Assert.AreEqual("5,1:hello", output);
+            string result = "";
+            int length = input.Length;
+            int words = input.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).Length;
+
+            result = length + "," + words + ":" + input;
+
+            return result;
         }
 
-        [Test]
-        public void Test2()
-        {
-            Program p = new Program();
-            string output = p.Prefix("");
-            Assert.AreEqual("0,0:", output);
-        }
 
-        [Test]
-        public void Test3()
+        //[Test]
+        //public void Test1()
+        //{
+        //    Program p = new Program();
+        //    string output = p.Prefix("hello");
+        //    Assert.AreEqual("5,1:hello", output);
+        //}
+
+        //[Test]
+        //public void Test2()
+        //{
+        //    Program p = new Program();
+        //    string output = p.Prefix("");
+        //    Assert.AreEqual("0,0:", output);
+        //}
+
+        //[Test]
+        //public void Test3()
+        //{
+        //    Program p = new Program();
+        //    string output = p.Prefix("what    ...  did you say?? ");
+        //    Assert.AreEqual("27,5:what    ...  did you say?? ", output);
+        //}
+    }
+
+    public class MyTestClass
+    {
+        public static IEnumerable TestCases
         {
-            Program p = new Program();
-            string output = p.Prefix("what    ...  did you say?? ");
-            Assert.AreEqual("27,5:what    ...  did you say?? ", output);
+            get
+            {
+                yield return new TestCaseData("hello").Returns("5,1:hello");
+                yield return new TestCaseData("").Returns("0,0:");
+                yield return new TestCaseData("what    ...  did you say?? ").Returns("27,5:what    ...  did you say?? ");
+            }
         }
     }
 }
